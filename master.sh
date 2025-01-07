482a2f7a91f49147f05455730eb84b98dd2b76b6
projecteinf <96139692+projecteinf@users.noreply.github.com>
2025-01-07 10:08:31 +0100

    Actualització a versió 1.32 de Kubernetes

--- master.sh
+++ master.sh
@@ -81,9 +81,9 @@ echo "SUCCESS: fitxer hosts configurat de forma correcte"
 
 apt-get update && apt-get install -y apt-transport-https curl
 
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | tee /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.32/deb/ /' | tee /etc/apt/sources.list.d/kubernetes.list
 
 apt-get update
 
